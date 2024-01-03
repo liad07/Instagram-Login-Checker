b5196428757340d0047265095c2892b94623d516
liadk07 <91607695+liad07@users.noreply.github.com>
2024-01-03 09:22:31 +0200

    Update main.py
    fix some problems

--- main.py
+++ main.py
@@ -20,14 +20,18 @@ def IsExists(user,password):
 
   response = requests.request("POST", url, headers=headers, data=payload, files=files)
   csrf=response.cookies["csrftoken"]
+  mid=response.cookies["mid"]
+  ig_did=response.cookies["ig_did"]
+  ig_nrcb=response.cookies["ig_nrcb"]
   headers = {
     'X-Csrftoken': f'{csrf}',
-    'Cookie': f"csrftoken={csrf}; mid=ZIrEtgALAAE7GrCUwQ9wcQbbrefW; ig_did=80445D30-C9F9-4D3F-8BF0-78B39275775C; ig_nrcb=1; datr=tcSKZFMeDkyjVKNghYr_9-WI"
+    'Cookie': f"csrftoken={csrf}; mid={mid}; ig_did={ig_did}; ig_nrcb={ig_nrcb};"
   }
   response = requests.request("POST", url, headers=headers, data=payload, files=files)
   return  (response.json())
 user=input("enter user\n")
 password=input("enter password\n")
+
 x=IsExists(user,password)
 if x["status"]=="ok" and x["authenticated"]!=None and x["authenticated"]==True:
   print("user and password matching ")
